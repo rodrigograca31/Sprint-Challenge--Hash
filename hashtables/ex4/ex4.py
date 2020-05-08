adbf0835667dabc7ceb70addc9545dac52f3ba5b
Rodrigo Graça <rodrigograca31@gmail.com>
2020-05-08 11:14:29 +0100

    feat(ex4): ✨ Exercise 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,23 @@
 def has_negatives(a):
-
     """
     YOUR CODE HERE
     """
+
+    hashtable = {}
+    result = []
+
+    for index, number in enumerate(a):
+        # opposite = -number
+
+        if -number in hashtable:
+            result.append(abs(number))
+
+        hashtable[number] = number
 
     return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1,-2,1,2,3,4,-4]))
+    # print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    # print()
+    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
